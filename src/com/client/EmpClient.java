3fc1403858b79b0c0804a2c28c4bbdc66fb5ae44
NIKHITHA160 <nikhithabalabhadra@gmail.com>
2025-04-08 12:20:21 +0530

    added display method

--- src/com/client/EmpClient.java
+++ src/com/client/EmpClient.java
@@ -4,4 +4,7 @@ public class EmpClient {
     public static void main(String[] args) {
         System.out.println("welcome");
     }
+    public static void display(){
+        System.out.println("Display");
+    }
 }
